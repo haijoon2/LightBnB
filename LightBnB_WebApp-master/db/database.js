cc14b00a7818639274c4638bb609a84576a2e56f
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-09-04 19:13:14 -0700

    refactor getAllReservations function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -60,7 +60,22 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT 
+    reservations.*, 
+    properties.*, 
+    AVG(property_reviews.rating) AS average_rating
+  FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP BY reservations.id, properties.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit]).then(response =>
+    response.rows
+  ).catch((error) => {
+    console.log(error);
+  });
 };
 
 /// Properties
